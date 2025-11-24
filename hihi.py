1fe946b83c4969041f4a2444ae9e5fde8973061c
20352552 <rkemp39@wgu.edu>
2025-11-23 22:02:50 -0800

    Testing out pycharms github integration

---
+++ hihi.py
@@ -0,0 +1,11 @@
+## name = input("What is your pets name?")
+## print("It is time for " + name + "'s medication")
+
+
+print(type(2 +4))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4))
+print(type(0))
+print(bin(5))
+print(int('0b101', 2))
