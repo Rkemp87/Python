2132bc49ae8c1f2378873bb6a63bccdb3150c860
20352552 <rkemp39@wgu.edu>
2025-11-27 00:39:00 -0800

    further string functions and a small example of password checker

--- strings.py
+++ strings.py
@@ -30,3 +30,29 @@ print(lets_change_the_order.format(days_of_study, goal_days_of_study))
 
 lets_change_the_order = 'My goal is study for {new_goal_days_of_study} days. My current streak is {new_days_of_study}.'
 print(lets_change_the_order.format(new_days_of_study = 6, new_goal_days_of_study = 105))
+
+##expected outcome is 'My go'
+print(lets_change_the_order[0:5])
+
+##stepover - expected outcome is 'M o'
+print(lets_change_the_order[0:5:2])
+
+## no end - expected outcome 'goal is study for...'
+print(lets_change_the_order[3:])
+
+## no start - expected outcome 'my goal is study'
+print(lets_change_the_order[:16])
+
+print(lets_change_the_order.upper())
+
+print(lets_change_the_order.lower())
+
+print(lets_change_the_order.swapcase())
+
+print(lets_change_the_order.title())
+print(lets_change_the_order.capitalize())
+
+birth_year= int(input('What year were you born?'))
+age = 2025 - birth_year
+
+print(f'You are {age} years old.')
